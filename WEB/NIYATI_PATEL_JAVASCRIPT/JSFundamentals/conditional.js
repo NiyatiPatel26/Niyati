de47f0e34fcb0c142b1a923fc59e16173c7f3ab1
NiyatiPatel26 <89980939+NiyatiPatel26@users.noreply.github.com>
2021-09-13 10:17:24 +0000

    Add files via upload

---
+++ WEB/NIYATI_PATEL_JAVASCRIPT/JSFundamentals/conditional.js
@@ -0,0 +1,17 @@
+let age = prompt('age?', 18);
+//Conditional Operator
+/*let message = (age < 3) ? 'Hi, baby!' :
+  (age < 18) ? 'Hello!' :
+  (age < 100) ? 'Greetings!' :
+  'What an unusual age!';*/
+
+if (age < 3) {
+    message = 'Hi, baby!';
+  } else if (age < 18) {
+    message = 'Hello!';
+  } else if (age < 100) {
+    message = 'Greetings!';
+  } else {
+    message = 'What an unusual age!';
+  }
+alert( message );
